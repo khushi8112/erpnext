b75b9599afd4165e1e89a838c8b768c0d530498b
Thomas antony <77287334+thomasantony12@users.noreply.github.com>
2026-02-06 12:48:15 +0530

    fix: Add handling for Sales Invoice Item quantity field
    Add handling for Sales Invoice Item quantity field
    
    (cherry picked from commit edfcaee99b19906191e2c1c7db44ce11966f1833)

--- erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
+++ erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
@@ -1032,6 +1032,8 @@ class SerialandBatchBundle(Document):
 			qty_field = "consumed_qty"
 		elif row.get("doctype") == "Stock Entry Detail":
 			qty_field = "transfer_qty"
+		elif row.get("doctype") == "Sales Invoice Item":
+			qty_field = "stock_qty"
 
 		return qty_field
 
