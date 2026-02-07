82843f49bf6d398093890eaf72c33090f5bf4d13
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-02-07 11:16:35 +0530

    fix(stock): ignore pos reserved batches for stock levels
    (cherry picked from commit 277ba9cb794b1a979a9b3a59208f56679c20faf3)

--- erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
+++ erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
@@ -2549,7 +2549,10 @@ def get_auto_batch_nos(kwargs):
 	qty = flt(kwargs.qty)
 
 	stock_ledgers_batches = get_stock_ledgers_batches(kwargs)
-	pos_invoice_batches = get_reserved_batches_for_pos(kwargs)
+
+	pos_invoice_batches = frappe._dict()
+	if not kwargs.for_stock_levels:
+		pos_invoice_batches = get_reserved_batches_for_pos(kwargs)
 
 	sre_reserved_batches = frappe._dict()
 	if not kwargs.ignore_reserved_stock:
