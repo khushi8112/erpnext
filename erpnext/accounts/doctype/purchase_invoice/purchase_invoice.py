956ac90790695d9f66a1f5d3c3eb54a2b127df1d
Mihir Kandoi <kandoimihir@gmail.com>
2026-02-21 12:08:37 +0530

    fix: remove supplier invoice date/posting date validation
    (cherry picked from commit 7cff0ba6266297092a4c678aa5d1fc40877d61df)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -1729,10 +1729,6 @@ class PurchaseInvoice(BuyingController):
 			project_doc.db_update()
 
 	def validate_supplier_invoice(self):
-		if self.bill_date:
-			if getdate(self.bill_date) > getdate(self.posting_date):
-				frappe.throw(_("Supplier Invoice Date cannot be greater than Posting Date"))
-
 		if self.bill_no:
 			if cint(frappe.db.get_single_value("Accounts Settings", "check_supplier_invoice_uniqueness")):
 				fiscal_year = get_fiscal_year(self.posting_date, company=self.company, as_dict=True)
